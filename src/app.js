e08b89e563684a020f60a667e33c344d2be0586d
Jackie Curtsinger <jackie_curtsinger@yahoo.com>
2020-08-20 10:37:05 -0700

    set paths with handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000 //THIS IS TO GET THE PORT FROM THE SYSTEM ITS RUNNIGN ON
+
+const publicStaticDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials'); //FOR MULTIPLE VIEWS
+
+
+app.set('view engine', 'hbs') //LET EXPRESS KNOW THAT THE TEMPLATES ARE COMING FROM HANDLEBARS
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicStaticDirPath));
 
 app.get('', (req, res) => {
     res.send("Main page of App");
